17fb1e1a9f95c394be99daa51fb88371e01d743a
CleroStyle <glazacevnikolaj7@gmail.com>
2023-03-30 01:14:54 +0300

    Add auth template

---
+++ BoardingAppAPI/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoardingAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly BoardingAppContext _context;
+
+        public AuthController(BoardingAppContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+    }
+}
