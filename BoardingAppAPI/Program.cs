bb817cc9efd923637b37378b02ac22058becfe6b
CleroStyle <glazacevnikolaj7@gmail.com>
2023-03-30 01:07:26 +0300

    Add usersecrets

--- BoardingAppAPI/Program.cs
+++ BoardingAppAPI/Program.cs
@@ -9,12 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
-});
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<BoardingAppContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
