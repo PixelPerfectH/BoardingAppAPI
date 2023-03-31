ff44242739b8a96089963f2ad38563208367ee0c
CleroStyle <glazacevnikolaj7@gmail.com>
2023-03-31 21:11:21 +0300

    Auth update

--- BoardingAppAPI/Controllers/AuthController.cs
+++ BoardingAppAPI/Controllers/AuthController.cs
@@ -16,26 +16,18 @@ namespace BoardingAppAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        private const string RefreshTokenKey = "RefreshToken";
         private readonly ILogger<AuthController> _logger;
         private readonly BoardingAppContext _context;
-        private readonly IJwtUtils _jwtUtils;
-        private readonly AppSettings _appSettings;
 
-        public AuthController(BoardingAppContext context, IJwtUtils jwtUtils, AppSettings appSettings, ILogger<AuthController> logger)
+        public AuthController(BoardingAppContext context, ILogger<AuthController> logger)
         {
             _logger = logger;
             _context = context;
-            _jwtUtils = jwtUtils;
-            _appSettings = appSettings;
         }
 
         [HttpPost("[action]")]
-        [AllowAnonymous]
         [Consumes(MediaTypeNames.Application.Json)]
         [Produces(MediaTypeNames.Application.Json)]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LoginResult))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(LoginError))]
         public async Task<IActionResult> LoginByPasswordAsync([FromBody] LoginByPasswordModel model, CancellationToken cancellationToken)
         {
             var user = await _context.Users
@@ -46,158 +38,25 @@ namespace BoardingAppAPI.Controllers
                 return NotFound();
             }
 
-            var jwtToken = _jwtUtils.GenerateJwtToken(user);
-            var refreshToken = _jwtUtils.GenerateRefreshToken(GetIp());
-            user.RefreshTokens.Add(refreshToken);
-
-            user.RefreshTokens.RemoveAll(token => !token.IsActive);
-
-            _context.Update(user);
-            await _context.SaveChangesAsync(cancellationToken);
-
-            SetTokenCookie(refreshToken.Token);
-
-            return Ok(new LoginResult()
-            {
-                Jwt = jwtToken
-            });
+            return Ok();
         }
 
         [HttpPost("[action]")]
-        [AllowAnonymous]
+        [Consumes(MediaTypeNames.Application.Json)]
         [Produces(MediaTypeNames.Application.Json)]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LoginResult))]
-        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(RefreshTokenLoginError))]
-        public async Task<IActionResult> LoginByRefreshTokenAsync([FromBody] LoginByTokenModel model, CancellationToken cancellationToken)
-        {
-            var token = Request.Cookies[RefreshTokenKey];
-
-            var user = await _context.Users.Include(u => u.RefreshTokens).FirstOrDefaultAsync(u => u.RefreshTokens.Any(t => t.Token == token), cancellationToken);
-
-            if (user is null)
-            {
-                return BadRequest(RefreshTokenLoginError.InvalidToken);
-            }
-
-            var refreshToken = user.RefreshTokens.First(u => u.Token == token);
-
-            if (refreshToken.IsRevoked)
-            {
-                RevokeDescendantRefreshTokens(refreshToken, user, GetIp(), $"Attempted reuse of revoked ancestor token: {token}");
-                _context.Update(user);
-                await _context.SaveChangesAsync(cancellationToken);
-            }
-
-            if (!refreshToken.IsActive)
-            {
-                return BadRequest(RefreshTokenLoginError.InvalidToken);
-            }
-
-            var newRefreshToken = RotateRefreshToken(refreshToken, GetIp());
-
-            user.RefreshTokens.Add(newRefreshToken);
-            user.RefreshTokens.RemoveAll(t => !t.IsActive);
-
-            _context.Update(user);
-            await _context.SaveChangesAsync(cancellationToken);
-
-            var jwt = _jwtUtils.GenerateJwtToken(user);
-
-            SetTokenCookie(newRefreshToken.Token);
-
-            return Ok(new LoginResult
-            {
-                Jwt = jwt
-            });
-        }
-
-        [HttpPost("[action]")]
         public async Task<IActionResult> RegisterAsync([FromBody] RegisterModel model, CancellationToken cancellationToken)
         {
-            var tokens = new List<RefreshToken>();
-            var refreshToken = _jwtUtils.GenerateRefreshToken(GetIp());
-
-            tokens.Add(refreshToken);
-
             var user = new DBUser()
             {
                 UserName = model.UserName,
-                RefreshTokens = tokens,
                 HashedPassword = BCrypt.Net.BCrypt.HashPassword(model.Password),
                 CreatedAt = DateTime.UtcNow
             };
 
-            var jwt = _jwtUtils.GenerateJwtToken(user);
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
 
-            await _context.AddAsync(user, cancellationToken);
-            await _context.SaveChangesAsync(cancellationToken);
-
-            SetTokenCookie(refreshToken.Token);
-
-            return Ok(new RegisterResult()
-            {
-                Jwt = jwt
-            });
-        }
-
-        private string GetIp()
-        {
-            if (Request.Headers.ContainsKey("X-Forwarded-For"))
-            {
-                return Request.Headers["X-Forwarded-For"]!;
-            }
-
-            var ipAddress = HttpContext.Connection.RemoteIpAddress?.ToString();
-
-            if (ipAddress is null)
-            {
-                _logger.LogDebug("HttpContext.Connection.RemoteIpAddress is null");
-            }
-
-            return ipAddress ?? string.Empty;
-        }
-
-        private void SetTokenCookie(string token)
-        {
-            var cookieOptions = new CookieOptions
-            {
-                HttpOnly = true,
-                Expires = DateTime.UtcNow.AddDays(_appSettings.RefreshTokenTtl),
-                Secure = true,
-                SameSite = SameSiteMode.None
-            };
-            Response.Cookies.Append(RefreshTokenKey, token, cookieOptions);
-        }
-
-        private void RevokeDescendantRefreshTokens(RefreshToken refreshToken, DBUser user, string ipAddress, string reason)
-        {
-            if (!string.IsNullOrEmpty(refreshToken.ReplacedByToken))
-            {
-                var childToken = user.RefreshTokens.Single(x => x.Token == refreshToken.ReplacedByToken);
-                if (childToken.IsActive)
-                {
-                    RevokeRefreshToken(childToken, ipAddress, reason);
-                }
-                else
-                {
-                    RevokeDescendantRefreshTokens(childToken, user, ipAddress, reason);
-                }
-            }
-        }
-
-        private static void RevokeRefreshToken(RefreshToken token, string ipAddress, string? reason = null, string? replacedByToken = null)
-        {
-            token.Revoked = DateTime.UtcNow;
-            token.RevokedByIp = ipAddress;
-            token.ReasonRevoked = reason;
-            token.ReplacedByToken = replacedByToken;
-        }
-
-        private RefreshToken RotateRefreshToken(RefreshToken refreshToken, string ipAddress)
-        {
-            var newRefreshToken = _jwtUtils.GenerateRefreshToken(ipAddress);
-            RevokeRefreshToken(refreshToken, ipAddress, "Replaced by new token", newRefreshToken.Token);
-            return newRefreshToken;
+            return Ok();
         }
     }
 }
--- BoardingAppAPI/Models/Database/DBUser.cs
+++ BoardingAppAPI/Models/Database/DBUser.cs
@@ -6,50 +6,23 @@ namespace BoardingAppAPI.Models.Database
     {
         public DBUser() { }
 
-
         [Key]
         public long Id { get; set; }
 
-        /// <summary>
-        /// UserName.
-        /// </summary>
         [Required]
         public required string? UserName { get; set; }
 
-        /// <summary>
-        /// User's email.
-        /// </summary>
         public string? Email { get; set; }
 
-        /// <summary>
-        /// BCrypt hashed password.
-        /// </summary>
         [Required]
         public required string? HashedPassword { get; set; }
 
-        /// <summary>
-        /// User's firstname.
-        /// </summary>
         public string? FirstName { get; set; }
 
-        /// <summary>
-        /// User's lastname.
-        /// </summary>
         public string? LastName { get; set; }
 
-        /// <summary>
-        /// User's avatar.
-        /// </summary>
         public byte[]? Avatar { get; set; }
 
-        /// <summary>
-        /// Account creation date.
-        /// </summary>
         public required DateTime CreatedAt { get; set; }
-
-        /// <summary>
-        /// List of user's refresh tokens.
-        /// </summary>
-        public required List<RefreshToken> RefreshTokens { get; set; }
     }
 }
