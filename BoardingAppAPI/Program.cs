b5dc8ad5ffc52c983161b0893e7d54e28f7a6084
CleroStyle <glazacevnikolaj7@gmail.com>
2023-03-31 21:13:11 +0300

    Update

--- BoardingAppAPI/Program.cs
+++ BoardingAppAPI/Program.cs
@@ -1,5 +1,4 @@
 using BoardingAppAPI;
-using BoardingAppAPI.Tokens;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,8 +10,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<BoardingAppContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
-builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
-builder.Services.AddScoped<IJwtUtils, JwtUtils>();
 
 var app = builder.Build();
 
