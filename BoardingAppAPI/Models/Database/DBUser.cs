5fbfee1d91753798f9481273423bfc7f946800bd
CleroStyle <glazacevnikolaj7@gmail.com>
2023-04-01 13:56:56 +0300

    Update tasks when user registers

--- BoardingAppAPI/Models/Database/DBUser.cs
+++ BoardingAppAPI/Models/Database/DBUser.cs
@@ -11,31 +11,36 @@ namespace BoardingAppAPI.Models.Database
             {
                 Name = "Task1",
                 Description = "Description 1",
-                IsActive = true
+                IsActive = true,
+                Level = 5
             };
             var task2 = new DBTask()
             {
                 Name = "Task2",
                 Description = "Description 2",
-                IsActive = true
+                IsActive = true,
+                Level = 5
             };
             var task3 = new DBTask()
             {
                 Name = "Task3",
                 Description = "Description 3",
-                IsActive = true
+                IsActive = true,
+                Level = 5
             };
             var task4 = new DBTask()
             {
                 Name = "Task4",
                 Description = "Description 4",
-                IsActive = true
+                IsActive = true,
+                Level = 5
             };
             var task5 = new DBTask()
             {
                 Name = "Task5",
                 Description = "Description 5",
-                IsActive = true
+                IsActive = true,
+                Level = 5
             };
             Tasks = new()
             {
